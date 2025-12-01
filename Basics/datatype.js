6dd407c398b9f1d10878ff57a280f3eaba0ccb36
Sushant Paikrao <35286203+Sushantplive@users.noreply.github.com>
2025-12-01 11:46:48 +0000

    variable and datatypes

---
+++ Basics/datatype.js
@@ -0,0 +1,24 @@
+"use strict" // everything as newer version
+
+console.log('sushant paikarao')
+
+let name = "Sushant"
+let age = 18   
+let isLogged  = true
+
+/* 
+primitive :
+number 
+bugint
+string
+boolean
+null -> its a value
+undefined -> value has not been assigned
+symbol  -> used for unique ness
+*/
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+// object
+
---
+++ Basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 123456
+let accountEmail = "sushant@google.com"
+var accountPwd = "123"
+accountCity = 'Pune'
+
+let accountState;
+// accountId = 896 not allowed
+console.table([accountId,accountEmail,accountPwd,accountCity, accountState])
