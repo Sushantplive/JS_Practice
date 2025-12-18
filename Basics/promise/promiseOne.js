c67eee7a1985098248e5e9b227c12066cb079ad9
Sushant Paikrao <35286203+Sushantplive@users.noreply.github.com>
2025-12-18 16:34:30 +0000

    event loop information

--- Basics/promise/promiseOne.js
+++ Basics/promise/promiseOne.js
@@ -46,4 +46,31 @@ promiseTwo.then(function(){
 // promise1  setTimeout run 1000 ms
 // promise 2 second timeout call after 1 ms
 
+/**
+ * javascript execute code in phases
+ * 1) synchronous
+ * 2) microtask : High priority task that run immediatly after synchronous code run
+ *                but before any macrotask
+ *                They are process in a queue (first in first out) untill queue is empty
+ *                ex: 1) promise callback (.then(),.catch(),.finally())
+ *                      queMicrotask function()
+ *                      MutationObserver
+ *                      process.nextTick in nodejs
+ * 3) macrotask  : 
+ *                It is also called a task, a lower priority task
+ *                It is schedule to run later
+ *                They are process one at a time and only run after all microtask are done
+ *                ex: setTimeout,setInterval
+ *                    UI events(ex.click,scroll)
+ *                     I/O operation File reading,network request
+ * */ 
 
+/**
+ * How Event loop works
+ * 
+ * step 1 : execute all synchronous code
+ * step 2 : Process all microtask in a queue
+ * step 3 :process one macrotask at a time
+ * step 4: repeat
+ * 
+ *  */
